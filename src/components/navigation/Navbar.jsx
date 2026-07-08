fe980da98dcbb488d7ce98d07c9d6a0ad4da3ab4
Stefano Rago <rswebline@gmail.com>
2026-07-08 19:49:42 +0200

    Sprint 5.1 - Mobile navigation

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,23 +1,62 @@
+import { useEffect, useState } from "react";
 import { navigationLinks } from "../../data/navigation";
 import Button from "../ui/Button";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsMenuOpen((currentValue) => !currentValue);
+  }
+
+  function closeMenu() {
+    setIsMenuOpen(false);
+  }
+
+  useEffect(() => {
+    document.body.classList.toggle("nav-open", isMenuOpen);
+
+    return () => {
+      document.body.classList.remove("nav-open");
+    };
+  }, [isMenuOpen]);
+
   return (
-    <nav className="navbar">
-      <a className="navbar__brand" href="/">
+    <nav className="navbar" aria-label="Navigazione principale">
+      <a className="navbar__brand" href="/" onClick={closeMenu}>
         Evocons
       </a>
 
-      <ul className="navbar__menu">
-        {navigationLinks.map((link) => (
-          <li key={link.href}>
-            <a href={link.href}>{link.label}</a>
-          </li>
-        ))}
-      </ul>
+      <button
+        className="navbar__toggle"
+        type="button"
+        aria-label={isMenuOpen ? "Chiudi menu" : "Apri menu"}
+        aria-expanded={isMenuOpen}
+        aria-controls="primary-navigation"
+        onClick={toggleMenu}
+      >
+        <span />
+        <span />
+        <span />
+      </button>
 
-      <div className="navbar__cta">
-        <Button href="/contatti">Parla con un consulente</Button>
+      <div
+        id="primary-navigation"
+        className={`navbar__panel ${isMenuOpen ? "is-open" : ""}`}
+      >
+        <ul className="navbar__menu">
+          {navigationLinks.map((link) => (
+            <li key={link.href}>
+              <a href={link.href} onClick={closeMenu}>
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="navbar__cta">
+          <Button href="/contatti">Parla con un consulente</Button>
+        </div>
       </div>
     </nav>
   );
